1cb916e5ce283ca7175408fc87c1d2e249df5a2f
ayshaaktersaima1 <ayshaaktersaima1@gmail.com>
2026-06-21 00:05:22 +0600

    completed google signup functionality

--- src/components/GoogleBtn.jsx
+++ src/components/GoogleBtn.jsx
@@ -1,13 +1,14 @@
 'use client';
 
+import { authClient } from '@/lib/auth-client';
 import { Button } from '@heroui/react';
 import { Icon } from '@iconify/react';
 
 export default function GoogleBtn({ label = 'Continue with Google' }) {
-    const handleGoogleLogin = () => {
-        console.log('Google login clicked');
-
-        // Later you will add Google auth logic here
+    const handleGoogleLogin = async () => {
+        const data = await authClient.signIn.social({
+            provider: "google",
+        });
     };
 
     return (
@@ -16,7 +17,7 @@ export default function GoogleBtn({ label = 'Continue with Google' }) {
                 type="button"
                 onPress={handleGoogleLogin}
                 variant="bordered"
-                className="h-14 w-full rounded-[16px] border border-white/20 bg-white/10 text-[15px] font-semibold text-white transition hover:border-white/40 hover:bg-white/15"
+                className="h-14 w-full rounded-4 border border-white/20 bg-white/10 text-[15px] font-semibold text-white transition hover:border-white/40 hover:bg-white/15"
             >
                 <Icon icon="devicon:google" className="text-[22px]" />
                 {label}
